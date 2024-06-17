de94512c59f8b01e9abd49a414727eafda6184f1
baattezu <147482395+baattezu@users.noreply.github.com>
2024-06-18 00:02:18 +0500

    nemogu

---
+++ auth-service/src/main/java/org/baattezu/authservice/model/User.java
@@ -0,0 +1,18 @@
+package org.baattezu.authservice.model;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+}
---
+++ auth-service/src/main/java/org/baattezu/authservice/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.baattezu.authservice.repositories;
+
+
+import org.baattezu.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
